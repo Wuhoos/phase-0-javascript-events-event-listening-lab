79e4c3af1b551a3a353726196b13ef9489fa7f00
David <wjw3298@gmail.com>
2023-07-05 11:09:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    function clickAlert() {
+    alert('I was Clicked'); 
+}
+    input.addEventListener('click', clickAlert);
 }
